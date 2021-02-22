761d593e843c5973f1e19338974a1eb9d20f530c
ahsanmahmood <aoneahsan@gmail.com>
2021-02-22 19:38:18 +0500

    fix: updated

--- app/Http/Controllers/Frontend/AuthController.php
+++ app/Http/Controllers/Frontend/AuthController.php
@@ -69,7 +69,7 @@ class AuthController extends AuthenticatedSessionController
     public function register(Request $request)
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            // 'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ["required", "string", "min:6", "confirmed"],
             "address" => ["required", "string"],
